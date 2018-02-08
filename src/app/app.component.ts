474c50f9138d06d88456674a7c53a4ded6f84606
Sean Nicholas Dieterle <nicholas-dieterle@web.de>
2018-02-08 10:42:04 +0100

    Minimal ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  swChecking = false
+
+  constructor(
+    private swUpdate: SwUpdate
+  ) { }
+
+  checkUpdate() {
+    this.swChecking = true
+    this.swUpdate.checkForUpdate()
+      .then(() => this.swChecking = false)
+      .catch(console.error)
+  }
 }
